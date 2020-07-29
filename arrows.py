ceb7b2ba6149cb5e99c2674d3c9c01239ed11576
Andreas Rosland <andreas@ros.land>
2020-07-28 21:55:04 -0500

    working program

--- arrows.py
+++ arrows.py
@@ -14,23 +14,45 @@ database = 'words.db'
 conn = create_connection(database)
 c = conn.cursor()
 
-c.execute('DROP TABLE IF EXISTS words')
-c.execute('CREATE TABLE words (adjective TEXT, noun TEXT)')
+# logic to populate db
+c.execute('DROP TABLE IF EXISTS adjective')
+c.execute('CREATE TABLE adjective (adjective TEXT)')
+
+c.execute('DROP TABLE IF EXISTS noun')
+c.execute('CREATE TABLE noun (noun TEXT)')
 
 with open('word_files/adjectives/28K_adjectives.txt', 'r') as f:
     for line in f:
         data = line.split()
-        c.execute('INSERT INTO words (adjective) VALUES (?)', (data[0],))
-        if 'str' in line:
-            break
+        c.execute('INSERT INTO adjective (adjective) VALUES (?)', (data[0],))
+        conn.commit()
 
-with open('word_files/adjectives/28K_adjectives.txt', 'r') as f:
+with open('word_files/nouns/91K_nouns.txt', 'r') as f:
     for line in f:
         data = line.split()
-        c.execute('INSERT INTO words (adjective) VALUES (?)', (data[0],))
-        if 'str' in line:
-            break
+        c.execute('INSERT INTO noun (noun) VALUES (?)', (data[0],))
+        conn.commit()
 
+#slow logic but is probably ok for now
+c.execute("select * from adjective")
+results = c.fetchall()
+maxadj = len(results)
 
-conn.commit()
+c.execute("select * from noun")
+results = c.fetchall()
+maxnoun = len(results)
+
+for _ in range(50):
+    radnum = randint(1, maxadj)
+    c.execute("select adjective FROM adjective WHERE rowid = "+str(radnum)+";")
+    results = c.fetchone()
+    adjective = results[0]
+
+    radnum = randint(1, maxnoun)
+    c.execute("select noun FROM noun WHERE rowid = "+str(radnum)+";")
+    results = c.fetchone()
+    noun = results[0]
+
+    print(adjective +" "+ noun)
+
 conn.close()
